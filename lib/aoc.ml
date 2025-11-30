a9e03296589c8404583ff23a76791a45534206de
Mateusz Pietrzak <mateusz.jerzy.pietrzak@gmail.com>
2025-11-30 13:39:57 +0100

    Rewrite the library to be more monadic

--- lib/aoc.ml
+++ lib/aoc.ml
@@ -1,26 +1,42 @@
-open Core
+open! Core
+
+let run solve_1 solve_2 parse_input =
+  let open Result.Let_syntax in
+
+  let%bind argv =
+    if Int.equal (Array.length (Sys.get_argv ())) 3 then
+      Ok (Sys.get_argv ())
+    else
+      Error "Please specify the input file path and the part number"
+  in
+
+  let%bind part =
+    argv.(2)
+    |> Int.of_string_opt
+    |> Result.of_option ~error:"Could not parse part number"
+  in
+
+  let file = argv.(1) in
+  let lines = In_channel.read_lines file in
+  let t1 = Time_ns.now () in
+
+  let%bind () =
+    match part with
+    | 1 -> Ok (solve_1 (parse_input lines))
+    | 2 -> Ok (solve_2 (parse_input lines))
+    | _ -> Error "Invalid part number"
+  in
+
+  let t2 = Time_ns.now () in
+  let elapsed = Time_ns.diff t2 t1 in
+  printf "Elapsed running time: %.6f\n" (Time_ns.Span.to_sec elapsed);
+
+  Ok ()
 
 let run_day solve_1 solve_2 parse_input =
-  if Array.length (Sys.get_argv ()) <> 3 then
-    printf
-      "Invalid usage: Please specify the input file path and the part number.\n"
-  else
-    let arg_part = Int.of_string_opt (Sys.get_argv ()).(2) in
-    match arg_part with
-    | None -> printf "Invalid usage: Could not read the part.\n"
-    | Some part -> (
-        let arg_file = (Sys.get_argv ()).(1) in
-        try
-          let lines = In_channel.read_lines arg_file in
-          let t1 = Time_ns.now () in
-          (match part with
-          | 1 -> solve_1 (parse_input lines)
-          | 2 -> solve_2 (parse_input lines)
-          | _ -> printf "Invalid usage: Part %d not allowed.\n" part);
-          let t2 = Time_ns.now () in
-          let elapsed = Time_ns.diff t2 t1 in
-          printf "Elapsed running time: %.6f\n" (Time_ns.Span.to_sec elapsed)
-        with Sys_error _ -> printf "Invalid usage: Could not read the file.\n")
+  match run solve_1 solve_2 parse_input with
+  | Ok () -> ()
+  | Error str -> printf "Error: %s\n" str
 
 let int_list_of_line line =
   let split_list = String.split_on_chars line ~on:[ ' '; '\n'; '\t' ] in
---
+++ lib/aoc.mli
@@ -0,0 +1,4 @@
+open! Core
+
+val run_day : ('a -> unit) -> ('a -> unit) -> (string list -> 'a) -> unit
+val int_list_of_line : string -> int list
