85bc0ef4a03282f7f976356c7d0a1a1d0fbca7aa
Mateusz Pietrzak <mateusz.jerzy.pietrzak@gmail.com>
2025-12-06 11:49:42 +0100

    Day 6 passing solution

--- day_06/main.ml
+++ day_06/main.ml
@@ -2,47 +2,41 @@ open! Core
 
 type op = Add | Mult
 
-let solve_1 (nums, ops) =
-  List.zip_exn nums ops
-  |> List.iter ~f:(fun (problem, op) ->
-      (match op with Add -> printf "Add: " | Mult -> printf "Mul: ");
-      List.iter problem ~f:(fun elem -> printf "%d " elem);
-      printf "\n");
+let solve_1 (cols, ops) =
   let res =
-    List.zip_exn nums ops
-    |> List.fold ~init:0 ~f:(fun acc (problem, op) ->
+    List.zip_exn cols ops
+    |> List.fold ~init:0 ~f:(fun acc (col, op) ->
+        let nums =
+          col |> List.map ~f:String.to_list
+          |> List.map ~f:(fun num ->
+              List.filter num ~f:(fun c -> not @@ Char.equal c ' '))
+          |> List.map ~f:String.of_char_list
+          |> List.map ~f:Int.of_string
+        in
         let func = match op with Add -> ( + ) | Mult -> ( * ) in
         let init = match op with Add -> 0 | Mult -> 1 in
-        let r = List.fold problem ~init ~f:func in
-        printf " -> %d\n" r;
+        let r = List.fold nums ~init ~f:func in
+
         acc + r)
   in
   printf "%d\n" res
 
-let solve_2 (nums, ops) =
+let solve_2 (cols, ops) =
   let res =
-    List.zip_exn nums ops
-    |> List.fold ~init:0 ~f:(fun acc (problem, op) ->
-        (match op with Add -> printf "Add: " | Mult -> printf "Mul: ");
-        let _func = match op with Add -> ( + ) | Mult -> ( * ) in
-        let _init = match op with Add -> 0 | Mult -> 1 in
-        let new_prob =
-          problem |> List.map ~f:Int.to_string
-          |> List.map ~f:(fun str ->
-              let len = String.length str in
-              let pref =
-                List.init (10 - len) ~f:(const '0') |> String.of_char_list
-              in
-              pref ^ str)
-          |> List.map ~f:String.to_list (*|> List.transpose_exn*)
+    List.zip_exn cols ops
+    |> List.fold ~init:0 ~f:(fun acc (col, op) ->
+        let nums =
+          col |> List.map ~f:String.to_list |> List.transpose_exn
+          |> List.map ~f:(fun num ->
+              List.filter num ~f:(fun c -> not @@ Char.equal c ' '))
           |> List.map ~f:String.of_char_list
-          (*|> List.map ~f:Int.of_string*)
+          |> List.map ~f:Int.of_string
         in
-        List.iter new_prob ~f:(fun elem -> printf "%s " elem);
-        printf "\n";
-        (* let r = List.fold new_prob ~init ~f:func in *)
-        (* printf " -> %d\n" r; *)
-        acc)
+        let func = match op with Add -> ( + ) | Mult -> ( * ) in
+        let init = match op with Add -> 0 | Mult -> 1 in
+        let r = List.fold nums ~init ~f:func in
+
+        acc + r)
   in
   printf "%d\n" res
 
@@ -51,16 +45,38 @@ let parse_input lines =
     let split_list = String.split_on_chars aux ~on:[ ' '; '\n'; '\t' ] in
     List.filter split_list ~f:(fun x -> match x with "" -> false | _ -> true)
   in
-  let nums =
-    List.take lines (List.length lines - 1)
-    |> List.map ~f:Aoc.int_list_of_line
-    |> List.transpose_exn
+  let spacing =
+    List.drop lines (List.length lines - 1)
+    |> List.hd_exn
+    |> String.split_on_chars ~on:[ '*'; '+' ]
+    |> List.map ~f:String.length
+    |> List.map ~f:(( + ) 1)
+    |> List.tl_exn
+    |> fun x -> List.take x (List.length x - 1)
   in
+  let rec take spacing lines =
+    match spacing with
+    | [] -> []
+    | x :: xs ->
+        let cur_col =
+          List.map lines ~f:(fun row ->
+              row |> String.to_list |> fun t -> List.take t (x - 1))
+          |> List.map ~f:String.of_char_list
+        in
+        let res_col =
+          List.map lines ~f:(fun row ->
+              row |> String.to_list |> fun t -> List.drop t x)
+          |> List.map ~f:String.of_char_list
+        in
+        cur_col :: take xs res_col
+  in
+  let columns = take spacing (List.take lines (List.length lines - 1)) in
   let ops =
     List.drop lines (List.length lines - 1)
     |> List.hd_exn |> string_list
     |> List.map ~f:(function "*" -> Mult | "+" -> Add | _ -> assert false)
+    |> fun f -> List.take f (List.length f - 1)
   in
-  (nums, ops)
+  (columns, ops)
 
 let () = Aoc.run_day solve_1 solve_2 parse_input
